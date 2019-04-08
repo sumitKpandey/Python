2c70f85b6c7fa8359ee306fcc13c45580e2e0632
sumitKpandey <49293090+sumitKpandey@users.noreply.github.com>
2019-04-08 17:37:30 -0400

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -1,37 +1,33 @@
 def findProduct(inputGrid, length):
     max_prod = 0
-    numberOfCombination = []
+    ctr = 0
     for i in range(length):
         for j in range(length-3):
             prod = inputGrid[i][j]*inputGrid[i][j+1]*inputGrid[i][j+2]
-            k = str(i)+str(j)+str(i)+str(j+1)+str(i)+str(j+2)
-            if k not in numberOfCombination:
-                numberOfCombination.append(k)
+            print(prod)
+            ctr += 1
             if prod > max_prod:
                 max_prod = prod
             prod = inputGrid[j][i]*inputGrid[j+1][i]*inputGrid[j+2][i]
-            k = str(j)+str(i)+str(j+1)+str(i)+str(j+2)+str(i)
-            if k not in numberOfCombination:
-                numberOfCombination.append(k)
+            print(prod)
+            ctr += 1
             if prod > max_prod:
                 max_prod = prod
     for i in range(length-3):
         for j in range(length-3):
             prod = inputGrid[i][j]*inputGrid[i+1][j+1]*inputGrid[i+2][j+2]
-            k = str(i)+str(j)+str(i+1)+str(j+1)+str(i+2)+str(j+2)
-            if k not in numberOfCombination:
-                numberOfCombination.append(k)
+            print(prod)
+            ctr += 1
             if prod > max_prod:
                 max_prod = prod
     for i in range(2, length):
         for j in range(length-3):
             prod = inputGrid[i][j]*inputGrid[i-1][j+1]*inputGrid[i-2][j+2]
-            k = str(i)+str(j)+str(i-1)+str(j+1)+str(i-2)+str(j+2)
-            if k not in numberOfCombination:
-                numberOfCombination.append(k)
+            print(prod)
+            ctr += 1
             if prod > max_prod:
                 max_prod = prod
-    print("Number of different combinations: ",len(numberOfCombination))
+    print("Number of different combinations: ",ctr)
     print("Greatest product of 3 adjacent numbers: ",max_prod)
 
 findProduct([[8,2,22,97,38,15,0,40,0,75],[49,49,99,40,17,81,18,57,60,87],[81,49,31,73,55,79,14,29,93,71],[52,70,95,23,4,60,11,42,69,24],[22,31,16,71,51,67,63,89,41,92],[24,47,32,60,99,3,45,2,44,75],[32,98,81,28,64,23,67,10,26,38],[67,26,20,68,2,62,12,20,95,63],[24,55,58,5,66,73,99,26,97,17],[21,36,23,9,75,0,76,44,20,45]],10)
